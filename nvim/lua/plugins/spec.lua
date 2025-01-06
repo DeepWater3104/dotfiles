98594ef6e19ffed2a94da5f507e1ae5ab47d4886
DeepWater3104 <redcurry1106@gmail.com>
2025-01-06 19:00:07 +0900

    update

--- nvim/lua/plugins/spec.lua
+++ nvim/lua/plugins/spec.lua
@@ -80,4 +80,23 @@ return {
       },
       version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
 }
